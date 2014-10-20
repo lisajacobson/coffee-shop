3d7a4b130780548842618eb820f0a68717272e51
lrsnider <lisarsnider@gmail.com>
2014-10-19 22:42:55 -0400

    progress w/js

---
+++ app/assets/javascripts/venues.js
@@ -0,0 +1,50 @@
+//Client id
+//HEF1ZRBBHNI532L2WNTOA0TMDDXL4RLG4E1ROV0M5V3SKJIY
+
+//Client secret
+//RFF1AAYFPZTHPC24OO0CKA3DN45P4J4LKVYCUHFFN4Z5NZYB
+
+
+//To make a userless request, specify your consumer key's Client ID and Secret instead of an auth token in the request URL.
+
+	var lat = $('.lat')
+	var lon = $('.lon')
+
+	//generating list of 10 nearest coffee shops using:
+		//lat + lon (only user input)
+		//radius = 50000 meters > 30mi
+		//intent=browse
+		//limit=10
+		//categoryId coffee shops
+
+$('button').on('click', function(event){
+
+	venueUrl = 'https://api.foursquare.com/v2/venues/search?client_id=HEF1ZRBBHNI532L2WNTOA0TMDDXL4RLG4E1ROV0M5V3SKJIY&client_secret=RFF1AAYFPZTHPC24OO0CKA3DN45P4J4LKVYCUHFFN4Z5NZYB&ll=' + lat + ',' + lon + '&limit=10&intent=browse&categoryId=4bf58dd8d48988d1e0931735&radius=60000&v=20141130&m=foursquare'
+	$.ajax({
+		type: 'GET',
+		url: venueUrl,
+		success: function(result){
+			console.log("woohoo!")
+		}
+	});
+
+	var shops_list = $('.list').hide();
+	$()
+})
+
+
+
+
+//test this out w/NYC location
+	//https://api.foursquare.com/v2/venues/search?client_id=HEF1ZRBBHNI532L2WNTOA0TMDDXL4RLG4E1ROV0M5V3SKJIY&client_secret=RFF1AAYFPZTHPC24OO0CKA3DN45P4J4LKVYCUHFFN4Z5NZYB&ll=40.7,-74&limit=10&intent=browse&categoryId=4bf58dd8d48988d1e0931735&radius=40000&v=20141130&m=foursquare
+//IT WORKS!
+
+//this url returns all resulting venues for query "coffee" in the city (default foursquare vicinity) 40.7, -74 (lat, lon) or Brooklyn Bridge Park Greenway Brooklyn, NY 11201
+//'https://api.foursquare.com/v2/venues/search?client_id=HEF1ZRBBHNI532L2WNTOA0TMDDXL4RLG4E1ROV0M5V3SKJIY&client_secret=RFF1AAYFPZTHPC24OO0CKA3DN45P4J4LKVYCUHFFN4Z5NZYB&ll=40.7,-74&query=coffee&v=20141130&m=foursquare'
+
+
+  //cvenues/categories
+  //food 4d4b7105d754a06374d81259
+  	//coffee shop 4bf58dd8d48988d1e0931735
+
+ $(document).ready(find);
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,12 @@
 class VenuesController < ActionController::Base
 
 	def index
+		@venues = Venue.all
+
+		respond_to do |format|
+			format.html
+			format.json {render json: @venues}
+		end
 	end
 
 	def show
